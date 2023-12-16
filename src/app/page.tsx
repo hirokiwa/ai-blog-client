3e6a0e98405bd892f07e387e29a17d1e8066dfe9
hirokiwa <hirokifsys@gmail.com>
2023-12-16 15:55:59 +0900

    add isPublished param

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,36 +5,42 @@ import Link from 'next/link';
 import getAllBlogs from './getAllBlogs';
 
 const Top = async () => {
-  const allBlogs = await getAllBlogs();
+  const isPublished = process.env.IS_PUBLISHED === "true";
+  const allBlogs = isPublished ? await getAllBlogs() : null;
   return (
     <main className={styles.main}>
-      {allBlogs
-        ? <div>
-          {allBlogs.map((a) => (
-            <Link href={`/post/${a.id}`} key={a.id}>
-              <div>
+      {isPublished && <>
+        { allBlogs
+          ? <div>
+            {allBlogs.map((a) => (
+              <Link href={`/post/${a.id}`} key={a.id}>
                 <div>
-                  <Image
-                    src={autherIcon}
-                    alt='AIおじさん'
-                    width={30}
-                    style={{
-                      backgroundColor: "gray",
-                      borderRadius: "50%",
-                    }}
-                    />
-                    <p>AIおじさん</p>
-                    <p>{ `${a.publishedAt.getFullYear()}/${a.publishedAt.getMonth()+1}/${a.publishedAt.getDate()} ${String(a.publishedAt.getHours()).padStart(2, '0')}:${String(a.publishedAt.getMinutes()).padStart(2, '0')}` }</p>
+                  <div>
+                    <Image
+                      src={autherIcon}
+                      alt='AIおじさん'
+                      width={30}
+                      style={{
+                        backgroundColor: "gray",
+                        borderRadius: "50%",
+                      }}
+                      />
+                      <p>AIおじさん</p>
+                      <p>{ `${a.publishedAt.getFullYear()}/${a.publishedAt.getMonth()+1}/${a.publishedAt.getDate()} ${String(a.publishedAt.getHours()).padStart(2, '0')}:${String(a.publishedAt.getMinutes()).padStart(2, '0')}` }</p>
+                  </div>
+                  <h2>{a.title}</h2>
+                    <p>{`${a.body.slice( 0, 30 )}...`}</p>
                 </div>
-                <h2>{a.title}</h2>
-                  <p>{`${a.body.slice( 0, 30 )}...`}</p>
-              </div>
-            </Link>
-          ))}
-        </div>
-        : <p>
-            <strong>記事が見つかりません。</strong>
-          </p>
+              </Link>
+            ))}
+          </div>
+          : <p>
+              <strong>記事が見つかりません。</strong>
+            </p>
+          }
+        </>}
+      {
+        !isPublished && <p>now developing...</p>
       }
     </main>
   )
