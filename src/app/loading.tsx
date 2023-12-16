23a5a256db3aa218eda7f7829f460fcaae324078
hirokiwa <hirokifsys@gmail.com>
2023-12-16 17:31:58 +0900

    add ';'

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import loadingAnimation from './../../public/loading.gif'
+import loadingAnimation from './../../public/loading.gif';
 
 const Loading = () => (
   <div style={{
