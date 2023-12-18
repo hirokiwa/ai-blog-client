dd386c2a2cca1e2e95ca4fb2853e695f2b4d2aec
hirokiwa <hirokifsys@gmail.com>
2023-12-18 22:13:12 +0900

    attach tailwindd

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,30 +2,17 @@ import Image from "next/image";
 import loadingAnimation from './../../public/loading.gif';
 
 const Loading = () => (
-  <div style={{
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  }}>
-    <div
-      style={{
-        textAlign: "center",
-      }}
-    >
-      <Image
-        src={loadingAnimation}
-        alt={"AIおじさん"}
-        width={300}
-      />
-      <hr/>
-      <p
-        style={{
-          margin: "1em",
-        }}
-      >loading...</p>
-    </div>
+  <div className="h-screen flex justify-center items-center">
+  <div className="text-center">
+    <Image
+      src={loadingAnimation}
+      alt="AIおじさん"
+      width={300}
+    />
+    <hr className="my-4" />
+    <p className="m-4">loading...</p>
   </div>
+</div>
 )
 
 export default Loading;
