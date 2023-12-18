bb46886953059f0f74f25a46fe5b99ddd5971630
hirokiwa <hirokifsys@gmail.com>
2023-12-19 00:06:55 +0900

    format top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,35 +5,55 @@ import Link from 'next/link';
 import getAllBlogs from './getAllBlogs';
 import loadingAnimation from './../../public/loading.gif';
 
+interface Props {
+  blogData: blog;
+}
+
+const BlogCard = ({blogData}: Props) => {
+  return (
+    <div className='my-8 mx-8'>
+      <Link
+        href={`/post/${blogData.id}`}
+        title={blogData.title}
+        className='my-8'
+        >
+        <div>
+          <div className='flex items-center'>
+            <Image
+              src={autherIcon}
+              alt='AIおじさん'
+              width={40}
+              height={40}
+              className="bg-gray-500 rounded-full mr-4"
+            />
+            <div>
+              <p  className=''>AIおじさん</p>
+              <p className='leading-0'>{`${blogData.publishedAt.getFullYear()}/${blogData.publishedAt.getMonth() + 1}/${blogData.publishedAt.getDate()} ${String(blogData.publishedAt.getHours()).padStart(2, '0')}:${String(blogData.publishedAt.getMinutes()).padStart(2, '0')}`}</p>
+            </div>
+          </div>
+          <h2 className='text-2xl font-bold my-4'>{blogData.title}</h2>
+          <p className='truncate'>{`${blogData.body}`}</p>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
 const Content = async () => {
   const allBlogs =  await getAllBlogs();
   return (
     <main className={styles.main}>
       <div className={styles.centerContent}>
         {allBlogs
-          ? <div>
-            {allBlogs.map((a) => (
-              <Link href={`/post/${a.id}`} key={a.id}>
-                <div>
-                  <div>
-                    <Image
-                      src={autherIcon}
-                      alt='AIおじさん'
-                      width={30}
-                      className="bg-gray-500 rounded-full"
-                    />
-                    <p>AIおじさん</p>
-                    <p>{`${a.publishedAt.getFullYear()}/${a.publishedAt.getMonth() + 1}/${a.publishedAt.getDate()} ${String(a.publishedAt.getHours()).padStart(2, '0')}:${String(a.publishedAt.getMinutes()).padStart(2, '0')}`}</p>
-                  </div>
-                  <h2 className='text-2xl font-bold mb-4'>{a.title}</h2>
-                  <p className='truncate'>{`${a.body}`}</p>
-                </div>
-              </Link>
-            ))}
-          </div>
+          ? allBlogs.map((a) => (
+            <div key={a.id}>
+              <BlogCard blogData={a} />
+              <hr/>
+            </div>
+            ))
           : <p>
-            <strong>記事が見つかりません。</strong>
-          </p>
+              <strong>記事が見つかりません。</strong>
+            </p>
         }
       </div>
     </main>
