c8b45089e85fd4beed97d5faa03bdd6f6ba12be8
hirokiwa <hirokifsys@gmail.com>
2023-12-20 01:19:45 +0900

    make header link active

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,25 @@
+import Link from 'next/link';
 import autherIcon from './../../public/icon.png';
 import Image from "next/image";
 
 const Header = () => (
-  <div className=''>
-    <div className='flex items-center ml-8'>
-      <Image
-        src={autherIcon}
-        alt='AIおじさん'
-        width={30}
-        height={30}
-        className="bg-gray-500 rounded-full mr-4"
-      />
-      <h1 className='text-2xl font-bold my-4'>AIおじさん毎日ブログ</h1>
+  <div>
+    <div className='inline-block'>
+      <Link
+        href='/'
+        className='flex items-center ml-8 hover:text-gray-400 hover:underline group'
+        title='トップページを読み込む'
+        aria-label='トップページを読み込む'
+      >
+        <Image
+          src={autherIcon}
+          alt='AIおじさん'
+          width={30}
+          height={30}
+          className="bg-gray-500 rounded-full mr-4 group-hover:shadow-md"
+        />
+        <h1 className='text-2xl font-bold my-4'>AIおじさん毎日ブログ</h1>
+      </Link>
     </div>
     <hr/>
   </div>
