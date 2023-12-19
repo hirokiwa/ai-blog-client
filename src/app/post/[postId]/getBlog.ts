5aa33cc416d07ad1925fb9c7612e2d0dd231e815
hirokiwa <hirokifsys@gmail.com>
2023-12-20 06:39:56 +0900

    fic time dirrerence

--- src/app/post/[postId]/getBlog.ts
+++ src/app/post/[postId]/getBlog.ts
@@ -5,11 +5,18 @@ const getBlog = async (blogId: string) => {
     const response = await fetch(`${process.env.HOST_NAME}/api/get-blog/${blogId}`);
     const result = await response.json();
     const formatedData = result.data ? result.data as blog : null;
+
+    const fixTimeDifference = (originalDate: Date) => {
+      const TIME_DIFFERENCE = 9;
+      const publishedDate = new Date(originalDate);
+      publishedDate.setHours(publishedDate.getHours() + TIME_DIFFERENCE);
+      return publishedDate;
+    }
     const blog = formatedData && {
       id: formatedData.id,
       title: formatedData.title,
       body: formatedData.body,
-      publishedAt: new Date(formatedData.publishedAt),
+      publishedAt: fixTimeDifference(formatedData.publishedAt),
     }
     return blog;
   } catch (e) {
