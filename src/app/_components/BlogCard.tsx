9e14cb26143108ea163556fd8dcbe4df82bb3dea
hirokiwa <hirokifsys@gmail.com>
2023-12-20 02:34:14 +0900

    add hoverd style

--- src/app/_components/BlogCard.tsx
+++ src/app/_components/BlogCard.tsx
@@ -12,26 +12,24 @@ const BlogCard = ({blogData}: Props) => {
       href={`/post/${blogData.id}`}
       title={blogData.title}
       aria-label={blogData.title}
-      className='py-8'
+      className='group m-0'
     >
-      <div className='my-8 mx-8'>
-        <div>
-          <div className='flex items-center'>
-            <Image
-              src={autherIcon}
-              alt='AIおじさん'
-              width={40}
-              height={40}
-              className="bg-gray-500 rounded-full mr-4"
-            />
-            <div>
-              <p  className=''>AIおじさん</p>
-              <p className='leading-0'>{`${blogData.publishedAt.getFullYear()}/${blogData.publishedAt.getMonth() + 1}/${blogData.publishedAt.getDate()} ${String(blogData.publishedAt.getHours()).padStart(2, '0')}:${String(blogData.publishedAt.getMinutes()).padStart(2, '0')}`}</p>
-            </div>
+      <div className='p-8 hoverdBackgroundColor group-hover:opacity-80'>
+        <div className='flex items-center'>
+          <Image
+            src={autherIcon}
+            alt='AIおじさん'
+            width={40}
+            height={40}
+            className="bg-gray-500 rounded-full mr-4"
+          />
+          <div>
+            <p  className=''>AIおじさん</p>
+            <p className='leading-0'>{`${blogData.publishedAt.getFullYear()}/${blogData.publishedAt.getMonth() + 1}/${blogData.publishedAt.getDate()} ${String(blogData.publishedAt.getHours()).padStart(2, '0')}:${String(blogData.publishedAt.getMinutes()).padStart(2, '0')}`}</p>
           </div>
-          <h2 className='text-2xl font-bold my-4'>{blogData.title}</h2>
-          <p className='truncate'>{`${blogData.body}`}</p>
         </div>
+        <h2 className='text-2xl font-bold my-4 group-hover:underline'>{blogData.title}</h2>
+        <p className='truncate'>{`${blogData.body}`}</p>
     </div>
     </Link>
   )
