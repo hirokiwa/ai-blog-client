1598c250693364eb40e4da772bcbf9a2832313b7
hirokiwa <hirokifsys@gmail.com>
2023-12-20 00:04:13 +0900

    fix indent

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,12 @@ const Header = () => (
   <div className=''>
     <div className='flex items-center ml-8'>
       <Image
-          src={autherIcon}
-          alt='AIおじさん'
-          width={30}
-          height={30}
-          className="bg-gray-500 rounded-full mr-4"
-        />
+        src={autherIcon}
+        alt='AIおじさん'
+        width={30}
+        height={30}
+        className="bg-gray-500 rounded-full mr-4"
+      />
       <h1 className='text-2xl font-bold my-4'>AIおじさん毎日ブログ</h1>
     </div>
     <hr/>
