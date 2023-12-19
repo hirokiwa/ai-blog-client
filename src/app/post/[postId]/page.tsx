fdd4c02d7eb28925397ff4df169fcba8b3d492b9
hirokiwa <hirokifsys@gmail.com>
2023-12-20 03:21:17 +0900

    add prof of AI guy

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -7,7 +7,7 @@ import formatContent from "@/functions/formatContent";
 const Content = ({ blogData }: { blogData: blog}) => (
   <div>
     <h2 className='text-2xl font-bold my-4'>{blogData.title}</h2>
-    <div className='flex items-center my-4'>
+    <div className='flex items-center mt-8 mb-16'>
       <Image
         src={autherIcon}
         alt='AIおじさん'
@@ -17,10 +17,26 @@ const Content = ({ blogData }: { blogData: blog}) => (
       />
       <div>
         <p  className=''>AIおじさん</p>
-        <p className='leading-0'>{`${blogData.publishedAt.getFullYear()}/${blogData.publishedAt.getMonth() + 1}/${blogData.publishedAt.getDate()} ${String(blogData.publishedAt.getHours()).padStart(2, '0')}:${String(blogData.publishedAt.getMinutes()).padStart(2, '0')}`}</p>
+        <p className='leading-0 opacity-60'>{`${blogData.publishedAt.getFullYear()}/${blogData.publishedAt.getMonth() + 1}/${blogData.publishedAt.getDate()} ${String(blogData.publishedAt.getHours()).padStart(2, '0')}:${String(blogData.publishedAt.getMinutes()).padStart(2, '0')}`}</p>
       </div>
     </div>
-    <p className="whitespace-pre-wrap">{formatContent(blogData.body)}</p>
+    <div className="mb-16">
+     <p className="whitespace-pre-wrap">{formatContent(blogData.body)}</p>
+    </div>
+    <hr/>
+    <div className='flex items-start mt-8 mb-16'>
+      <Image
+        src={autherIcon}
+        alt='AIおじさん'
+        width={60}
+        height={60}
+        className="bg-gray-500 rounded-full mr-4"
+      />
+      <div>
+        <p  className='text-lg'>AIおじさん</p>
+        <p className='leading-0 opacity-60'>大阪生まれ, 大阪育ち。大規模関西弁モデルのおじいちゃんです。人間とは思えない勤勉さで毎日19時にブログを更新中。</p>
+      </div>
+    </div>
   </div>
 )
 
