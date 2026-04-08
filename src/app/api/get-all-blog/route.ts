b4c2ef15f8b12201de1bd2d86ad7db2b546df42a
hirokiwa <hirokifsys@gmail.com>
2026-04-08 18:53:39 +0900

    Cache API response

--- src/app/api/get-all-blog/route.ts
+++ src/app/api/get-all-blog/route.ts
@@ -2,9 +2,10 @@ import { NextResponse } from 'next/server';
 
 import { getMockBlogs } from '../_functions/mock-data-provider/getMockBlogs';
 import { getPublishedBlogs } from '../_functions/blogs/blogs';
+import { createDailyResetCacheHeaders } from '../_functions/cache/cacheHeaders';
 
 export async function GET() {
   const useMockData = process.env.NEXT_PUBLIC_USE_MOCK === "true";
   const data = useMockData ? getMockBlogs() : await getPublishedBlogs();
-  return NextResponse.json({ data: data ?? undefined } );
+  return NextResponse.json({ data: data ?? undefined }, { headers: createDailyResetCacheHeaders() });
 }
